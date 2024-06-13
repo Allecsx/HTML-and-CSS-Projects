61def5a9a76bcbc1ad380660a351d1764fd2495f
Allecsx <alecss28@yahoo.com>
2024-06-13 08:45:36 +0100

    Create Hello_World.js

---
+++ Hello_World.js
@@ -0,0 +1,8 @@
+var greeting = 'Hello, World!';
+console.log(greeting);
+
+var greeting2 = 'Welcome TO Node.js';
+console.log(greeting2);
+
+var sentence1 = 'Welcome to Node.js';
+console.log(sentence1);
